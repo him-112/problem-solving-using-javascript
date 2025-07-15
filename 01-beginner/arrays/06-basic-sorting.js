03762d27d10a6449524572f303336893cc8846d1
him-bevolve <167058675+him-bevolve@users.noreply.github.com>
2025-07-15 21:55:35 +0530

    Add new search and sorting algorithms sample

--- 01-beginner/arrays/06-basic-sorting.js
+++ 01-beginner/arrays/06-basic-sorting.js
@@ -497,4 +497,88 @@ module.exports = {
     reverseArray,
     isSorted,
     partition
-}; 
+};
+
+function bubbleSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          // Swap
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        }
+      }
+    }
+    return arr;
+  }
+
+  function selectionSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n; i++) {
+      let minIdx = i;
+      for (let j = i + 1; j < n; j++) {
+        if (arr[j] < arr[minIdx]) {
+          minIdx = j;
+        }
+      }
+      // Swap
+      [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]];
+    }
+    return arr;
+  }
+
+  function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+      let key = arr[i];
+      let j = i - 1;
+  
+      // Peeche wale elements ko shift karo
+      while (j >= 0 && arr[j] > key) {
+        arr[j + 1] = arr[j];
+        j--;
+      }
+      arr[j + 1] = key;
+    }
+    return arr;
+  }
+
+  function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+  
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+  
+    return merge(left, right);
+  }
+  
+  function merge(left, right) {
+    let result = [];
+    let i = 0, j = 0;
+  
+    while (i < left.length && j < right.length) {
+      if (left[i] < right[j]) {
+        result.push(left[i]);
+        i++;
+      } else {
+        result.push(right[j]);
+        j++;
+      }
+    }
+    return result.concat(left.slice(i)).concat(right.slice(j));
+  }
+
+  function quickSort(arr) {
+    if (arr.length <= 1) return arr;
+  
+    let pivot = arr[arr.length - 1];
+    let left = [];
+    let right = [];
+  
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] < pivot) left.push(arr[i]);
+      else right.push(arr[i]);
+    }
+  
+    return [...quickSort(left), pivot, ...quickSort(right)];
+  }
